cac3673399df1f5ffbb6351df2bd4a7245ce19b8
unknown <govindvattikolla@gmail.com>
2026-04-09 17:08:43 +0530

    updated submodule

--- src/Api.js
+++ src/Api.js
@@ -1,3 +1,3 @@
-const BASE_URL = "https://geniemedia-in.onrender.com";
+const BASE_URL = "https://geniemedia-in-1.onrender.com";
 // const BASE_URL = "http://localhost:5000";
 export default BASE_URL;
